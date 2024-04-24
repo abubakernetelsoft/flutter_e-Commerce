934d6a97382eae2ded152ec13526ac413afd3b6b
abubakernetelsoft <119669788+abubakernetelsoft@users.noreply.github.com>
2024-04-24 13:27:23 +0500

    Design Login Screen

---
+++ lib/views/RegisterScreen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _RegisterScreen();
+
+}
+
+class _RegisterScreen extends State<RegisterScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
